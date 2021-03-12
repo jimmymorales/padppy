1a49f5eda7cd0c384af5350cfbf28b3ea145c505
Jimmy <me@jimmymorales.dev>
2021-03-11 20:41:07 -0600

    feature: show snackbar when adopting puppy

--- app/src/main/java/com/example/androiddevchallenge/ui/puppy_details/PuppyDetails.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/puppy_details/PuppyDetails.kt
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.puppy_details
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +26,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -19,8 +39,10 @@ import com.example.androiddevchallenge.ui.Screen
 import com.example.androiddevchallenge.ui.components.PadppyTopAppBar
 import com.example.androiddevchallenge.ui.components.description
 import com.example.androiddevchallenge.ui.components.icon
+import com.example.androiddevchallenge.ui.puppy_details.components.CongratsSnackbar
 import com.example.androiddevchallenge.ui.puppy_details.components.PuppyDescriptionSurface
 import dev.chrisbanes.accompanist.insets.statusBarsHeight
+import kotlinx.coroutines.launch
 
 const val puppyIdArg = "puppyId"
 
@@ -31,6 +53,9 @@ fun Screen.PuppyDetails.routeWithParam(puppyId: Long) =
 fun PuppyDetails(puppyId: Long, navigateBack: () -> Unit) {
     // TODO: Move this to VM
     val puppy = puppies.first { it.id == puppyId }
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     Scaffold {
         Column(modifier = Modifier.fillMaxSize()) {
@@ -45,6 +70,7 @@ fun PuppyDetails(puppyId: Long, navigateBack: () -> Unit) {
                 onBackPressed = navigateBack,
                 actions = {
                     Icon(
+                        modifier = Modifier.padding(horizontal = 16.dp),
                         imageVector = puppy.gender.icon,
                         contentDescription = puppy.gender.description,
                         tint = MaterialTheme.colors.primary,
@@ -62,9 +88,21 @@ fun PuppyDetails(puppyId: Long, navigateBack: () -> Unit) {
             PuppyDescriptionSurface(
                 modifier = Modifier.weight(weight = 1f, fill = true),
                 puppy = puppy,
-                onAdoptClicked = {},
+                onAdoptClicked = {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = "Congratulations! You have adopted ${puppy.name}"
+                        )
+                    }
+                },
             )
         }
     }
-}
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        CongratsSnackbar(
+            snackbarHostState = snackbarHostState,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
